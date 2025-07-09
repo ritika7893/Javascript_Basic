4acc6612552cdb22c8952283c0629ee7d22e21a2
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 12:55:39 +0530

    Create ArrayMethod.js

---
+++ ArrayMethod.js
@@ -0,0 +1,8 @@
+//forEach
+arr=[1,2,3,4,5,5,89,78,4,5]
+arr.forEach(num => console.log(num*num))
+//map
+//map
+arr=[1,2,3,4,5,5,89,78,4,5]
+const double=arr.map(num => num*num)
+console.log(double)
