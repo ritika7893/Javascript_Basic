2a85d5bd44b35a423f765c65a9e5d2dcaf52bd5a
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 13:49:25 +0530

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,18 @@
+const user={
+    name: "Priya",
+    age: 18,
+    is_drive:true,
+    fun:function(){
+        console.log("hello")
+    },
+    address: {
+    city: "Dehradun",
+    pin: 248001
+  }
+}
+console.log(user.name)
+console.log(user["age"])
+console.log(user.address.pin)
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
