70aa9744611e13a34aaecb1ac62e733850735659
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 12:57:32 +0530

    Update ArrayMethod.js

--- ArrayMethod.js
+++ ArrayMethod.js
@@ -6,3 +6,7 @@ arr.forEach(num => console.log(num*num))
 arr=[1,2,3,4,5,5,89,78,4,5]
 const double=arr.map(num => num*num)
 console.log(double)
+//filter
+arr=[1,2,3,4,5,5,89,78,4,5]
+const evens=arr.filter(num => num%2==0)
+console.log(evens)
