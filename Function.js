e78ada2c2aacfd00e517ce337e14cb06618b7e02
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 12:03:40 +0530

    Update Function.js

--- Function.js
+++ Function.js
@@ -16,3 +16,23 @@ function fibonacci(n){
 }
 console.log(greet())
 console.log(fibonacci(10))
+//Arrow Function
+const greet=(name)=>`hello,${name}`
+console.log(greet('cutie'))
+
+const add=(a,b)=>a+b
+console.log(add(8,7))
+const sub=(a,b)=>a-b
+console.log(sub(8,89))
+const pow=(x,y)=>x**y
+console.log(pow(2,5))
+
+const factorial=(num)=>{
+    let fact=1;
+    while(num>0){
+        fact=fact*num;
+        num=num-1;
+    }
+    return fact;
+};
+console.log(factorial(5))
