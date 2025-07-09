f617754e5e55787fed1603603b4897d6e815c5b0
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 14:03:50 +0530

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -16,3 +16,15 @@ console.log(user.address.pin)
 console.log(Object.keys(user))
 console.log(Object.values(user))
 console.log(Object.entries(user))
+
+
+const user = {
+  name: "Riya",
+  age: 21,
+  city: "Dehradun"
+};
+const { name, age}=user
+console.log(name)
+console.log(age)
+const { city:hometown}=user
+console.log(hometown)
