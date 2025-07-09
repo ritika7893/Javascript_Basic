3537be7de171d963db2ccc53da5b095f635f7011
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-09 11:16:22 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,15 @@
+/*for(let i=1;i<=5;i++){
+    console.log(i*i)
+}
+let fact=1
+let n=prompt("Enter the number")
+while(n>0){
+    fact=fact*n;
+    n=n-1;
+}
+console.log(fact)
+let fruits=["Apple","Mango","Banana"]
+for(let fruit of fruits){
+console.log(fruit)
+}
+*/
