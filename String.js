a0f8eae3457a323638259b2e01f4295c0b5abce6
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-07-10 10:51:47 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,28 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let a="Riya"
+let name="Ritika"
+let b=`hello,${a}`
+console.log(a)
+console.log(b)
+console.log(a.length)
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.indexOf('y'))
+console.log(name.indexOf('i'))
+console.log(name.lastIndexOf('i'))
+console.log(name.includes('iti'))
+console.log(name.startsWith('R'))
+console.log(name.startsWith('Ryiut'))
+console.log(name.slice(0,2))
+let newbee=name.substring(0, 3)
+console.log(newbee)
+let newname=name.replace('ka',newbee)
+console.log(newname)
+let msg = "  Hello world  ";
+msg=msg.trim()
+console.log(msg.trim());             // "Hello JavaScript"
+console.log(msg.includes("Java"));   // true
+console.log(msg.slice(2, 7));        // "Hello"
+console.log(msg.replace("Java", "ECMA")); // "  Hello ECMAScript  "
+console.log(msg.split(" "));   
